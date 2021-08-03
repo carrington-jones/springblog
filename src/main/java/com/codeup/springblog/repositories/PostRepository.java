06aff844e732c617ebda46310a4cd941426046fe
Carrington Jones <carrington.jones47@gmail.com>
2021-08-02 21:34:38 -0500

    Test exercises completed.

--- src/main/java/com/codeup/springblog/repositories/PostRepository.java
+++ src/main/java/com/codeup/springblog/repositories/PostRepository.java
@@ -8,4 +8,6 @@ import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findAllByUser(User user);
+
+    Post findByTitle(String title);
 }
--- src/test/java/com/codeup/springblog/SpringblogApplicationTests.java
+++ src/test/java/com/codeup/springblog/SpringblogApplicationTests.java
@@ -2,6 +2,7 @@ package com.codeup.springblog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.assertNotNull;
 
 @SpringBootTest
 class SpringblogApplicationTests {
